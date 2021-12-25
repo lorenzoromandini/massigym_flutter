7726216f26b918b4320d57b3123b1b7dd4d339b1
Lorenzo Romandini <lorenzoromandini@live.it>
2021-12-25 18:08:39 +0100

    Select profileImage

--- lib/ui/personal/profilo.dart
+++ lib/ui/personal/profilo.dart
@@ -1,8 +1,13 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:massigym_flutter/models/user_model.dart';
 import 'package:massigym_flutter/ui/auth/login_screen.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class Profilo extends StatefulWidget {
   const Profilo({Key? key}) : super(key: key);
@@ -14,6 +19,10 @@ class Profilo extends StatefulWidget {
 class _ProfiloState extends State<Profilo> {
   User? user = FirebaseAuth.instance.currentUser;
   UserModel loggedInUser = UserModel();
+  final storage = FirebaseStorage.instance;
+
+  String imageUrl = "";
+  int check = 0;
 
   @override
   void initState() {
@@ -28,6 +37,48 @@ class _ProfiloState extends State<Profilo> {
     });
   }
 
+  checkImage() async {
+    var snapshot = await storage.ref().child("profileImage/${user!.email}");
+    var downloadUrl = await snapshot.getDownloadURL();
+    if (downloadUrl != null) {
+      setState(() {
+        imageUrl = downloadUrl;
+      });
+    }
+    return imageUrl;
+  }
+
+  uploadImage() async {
+    final picker = ImagePicker();
+    PickedFile? image;
+
+    // Check permission
+    await Permission.photos.request();
+
+    var permissionStatus = await Permission.photos.request();
+
+    if (permissionStatus.isGranted) {
+      // select image
+      image = await picker.getImage(source: ImageSource.gallery);
+      var file = File(image!.path);
+      if (image != "") {
+        // upload to firebase
+        var snapshot = await storage
+            .ref()
+            .child("profileImage/${user!.email}")
+            .putFile(file);
+        var downloadUrl = await snapshot.ref.getDownloadURL();
+        setState(() {
+          imageUrl = downloadUrl;
+        });
+      } else {
+        print("No path received");
+      }
+    } else {
+      print("Grant Permissioms and try again");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,12 +86,6 @@ class _ProfiloState extends State<Profilo> {
       appBar: AppBar(
         title: Text("Profilo"),
         elevation: 0,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.red),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
         actions: <Widget>[
           IconButton(
               onPressed: () {
@@ -51,20 +96,27 @@ class _ProfiloState extends State<Profilo> {
       ),
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.all(20),
+          padding: const EdgeInsets.fromLTRB(40, 80, 40, 40),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               SizedBox(
-                height: 100,
-                child: Image.asset("assets/logo.png", fit: BoxFit.contain),
+                height: 250,
+                width: 400,
+                child: (checkImage() != "")
+                    ? Image.network(
+                        imageUrl,
+                        fit: BoxFit.contain,
+                      )
+                    : Image.asset("assets/logo.png", fit: BoxFit.contain),
               ),
-              const Text(
-                "Welcome back",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              SizedBox(
+                height: 50,
               ),
-              const SizedBox(height: 10),
+              ElevatedButton(
+                  onPressed: () => uploadImage(), child: Text("Upload image")),
+              const SizedBox(height: 40),
               Text("${loggedInUser.username}",
                   style: TextStyle(
                     color: Colors.black,
